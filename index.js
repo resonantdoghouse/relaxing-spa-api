316b3bbc5ce25e6ae7c6461d855c272a53dd4da6
Jim <jim@advancebass.com>
2021-02-22 20:46:39 -0800

    promisify quotes.json readFile

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const quotes = [
-  {
-    id: 1,
-    quote:
-      'You’re braver than you believe, stronger than you seem and smarter than you think',
-    source: 'Christopher Robin',
-  },
-  {
-    id: 2,
-    quote: 'People say nothing is impossible, but I do nothing every day.',
-    source: 'Winnie the Pooh',
-  },
-  {
-    id: 3,
-    quote: 'It never hurts to keep looking for sunshine.',
-    source: 'Eeyore',
-  },
-];
+async function readFile(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+
+async function loadQuotes() {
+  return await readFile('./data/quotes.json');
+}
 
 app.get('/', (req, res) => {
   res.json({
@@ -28,8 +25,15 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/quotes', (req, res) => {
-  res.json(quotes);
+app.get('/quotes', (_req, res) => {
+  loadQuotes()
+    .then((quotes) => {
+      res.send(quotes);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send('an error has occurred, check server for logs');
+    });
 });
 
 app.listen(PORT, () => {
